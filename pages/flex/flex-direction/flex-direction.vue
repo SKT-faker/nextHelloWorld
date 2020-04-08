c3f6bad2b01ed9a040e2800b463e0d6aa87403c2
杨如林 <18801301274@163.com>
2020-04-08 23:53:32 +0800

    flex的练习 flex容器的使用及换行排列方式的定义

---
+++ pages/flex/flex-direction/flex-direction.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="container">
+		<view class="green txt">
+			A
+		</view>
+		<view class="red txt">
+			B
+		</view>
+		<view class="blue txt">
+			C
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	@import url("flex-direction.css");
+</style>
