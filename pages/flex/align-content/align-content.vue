33f7db52a0c75fbfb821702f7603276d09a05fe9
杨如林 <18801301274@163.com>
2020-04-13 00:33:28 +0800

    04/13-1

---
+++ pages/flex/align-content/align-content.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="container">
+		<view class="green txt">
+			A
+		</view>
+		<view class="red txt">
+			B
+		</view>
+		<view class="blue txt">
+			C
+		</view>
+		<view class="orange txt">
+			E
+		</view>
+		<view class="yellow txt">
+			F
+		</view>
+		<view class="pink txt">
+			G
+		</view>
+		<view class="green txt">
+			A
+		</view>
+		<view class="red txt">
+			B
+		</view>
+		<view class="blue txt">
+			C
+		</view>
+		<view class="orange txt">
+			E
+		</view>
+		<view class="yellow txt">
+			F
+		</view>
+		<view class="pink txt">
+			G
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	@import url("align-content.css");
+</style>
---
+++ pages/flex/flex-wrap.1/flex-wrap.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="box">
+		<view class="blue txt">A</view>
+		<view class="orange txt">B</view>
+		<view class="green txt">C</view>
+		<view class="pink txt">D</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	@import url("flex-wrap.1.css");
+</style>
